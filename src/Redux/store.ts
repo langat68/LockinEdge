9da51106ea9b5761fc81a3acafd2d38e30d690e6
langat68 <langatfidel3@gmail.com>
2025-08-01 19:15:46 +0300

    added auto logout on inactivity

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,12 +1,14 @@
-
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import autoLogoutMiddleware from './autologoutmiddleware'; // Adjust path as needed
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     // Add other reducers here if you have them
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(autoLogoutMiddleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
